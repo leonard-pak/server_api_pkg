7f3736d8f5aa4a8199ea9d1f438b0d6a4053c8c4
Leonard Pak <leopak2000@gmail.com>
2024-03-28 16:24:22 +0300

    remove msg and srv to another pkg

--- src/robotic-arm-node.cpp
+++ src/robotic-arm-node.cpp
@@ -1,27 +1,27 @@
 #include "server_api_pkg/robot-arm-node.hpp"
 
-#include "server_api_pkg/GoToPoint.h"
+#include "common_msgs_srvs/GoToPoint.h"
 
 #include "ros/ros.h"
 #include "std_msgs/Float64MultiArray.h"
 
 namespace server_api_pkg
 {
+  namespace m_s = common_msgs_srvs;
   RoboticArmNode::RoboticArmNode()
       : mRoboticArm(std::make_unique<server_api_lib::RoboticArmController>(6)),
         mNode("robots/arm"),
         mPublisher(mNode.advertise<std_msgs::Float64MultiArray>("state", 1000)),
-        mService(
-            mNode.advertiseService<GoToPoint::Request, GoToPoint::Response>(
-                "go_to_point",
-                [this](GoToPoint::Request &req, GoToPoint::Response &res)
-                {
-                  ROS_INFO_STREAM("GoToPoint from robotic arm call");
-                  res.success = mRoboticArm.GoToPoint({.x = req.target.x,
-                                                       .y = req.target.y,
-                                                       .z = req.target.z});
-                  return true;
-                })),
+        mService(mNode.advertiseService<m_s::GoToPoint::Request,
+                                        m_s::GoToPoint::Response>(
+            "go_to_point",
+            [this](m_s::GoToPoint::Request &req, m_s::GoToPoint::Response &res)
+            {
+              ROS_INFO_STREAM("GoToPoint from robotic arm call");
+              res.success = mRoboticArm.GoToPoint(
+                  {.x = req.target.x, .y = req.target.y, .z = req.target.z});
+              return true;
+            })),
         mTimer(mNode.createTimer(ros::Duration(1),
                                  [this](const ros::TimerEvent &)
                                  {
--- src/two-wheel-robot-node.cpp
+++ src/two-wheel-robot-node.cpp
@@ -1,35 +1,35 @@
 #include "server_api_pkg/two-wheel-robot-node.hpp"
 
-#include "server_api_pkg/GoToPoint.h"
-#include "server_api_pkg/TwoWheelRobotState.h"
+#include "common_msgs_srvs/GoToPoint.h"
+#include "common_msgs_srvs/TwoWheelRobotState.h"
 
 #include "ros/ros.h"
 
 namespace server_api_pkg
 {
+  namespace m_s = common_msgs_srvs;
   TwoWheelRobotNode::TwoWheelRobotNode()
       : mTwoWheelRobot(
             std::make_unique<server_api_lib::TwoWheelRobotController>()),
         mNode("robots/two_wheel"),
-        mPublisher(mNode.advertise<TwoWheelRobotState>("state", 1000)),
-        mService(
-            mNode.advertiseService<GoToPoint::Request, GoToPoint::Response>(
-                "go_to_point",
-                [this](GoToPoint::Request &req, GoToPoint::Response &res)
-                {
-                  ROS_INFO_STREAM("GoToPoint from two wheel robot call");
-                  res.success = mTwoWheelRobot.GoToPoint({.x = req.target.x,
-                                                          .y = req.target.y,
-                                                          .z = req.target.z});
-                  return true;
-                })),
+        mPublisher(mNode.advertise<m_s::TwoWheelRobotState>("state", 1000)),
+        mService(mNode.advertiseService<m_s::GoToPoint::Request,
+                                        m_s::GoToPoint::Response>(
+            "go_to_point",
+            [this](m_s::GoToPoint::Request &req, m_s::GoToPoint::Response &res)
+            {
+              ROS_INFO_STREAM("GoToPoint from two wheel robot call");
+              res.success = mTwoWheelRobot.GoToPoint(
+                  {.x = req.target.x, .y = req.target.y, .z = req.target.z});
+              return true;
+            })),
         mTimer(mNode.createTimer(
             ros::Duration(1),
             [this](const ros::TimerEvent &)
             {
               ROS_INFO_STREAM("GetStatus from two wheel robot call");
               auto state = mTwoWheelRobot.GetState();
-              TwoWheelRobotState msg;
+              m_s::TwoWheelRobotState msg;
               msg.leftWheelVelocity = state.leftWheelVelocity;
               msg.rightWheelVelocity = state.rightWheelVelocity;
               mPublisher.publish(msg);
